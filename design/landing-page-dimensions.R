49b4bd957b249eee7bddd0af776559269640a9bd
Mohammad Savarmand <msavarmand@protonmail.com>
2023-12-11 09:22:17 -0500

    Overall dimensions of each section.

--- design/landing-page-dimensions.R
+++ design/landing-page-dimensions.R
@@ -1,3 +1,11 @@
+library(tidyverse)
 phi <- 1.618033988
 abs_tot_width <- 1400
-abs_tot_height <- 1400 * phi
+abs_tot_height <- 1400 + (1400*0.2) # 0.05 for header and 0.15 for footer
+
+
+# Heights
+header_height <- abs_tot_width * 0.05
+footer_height <- abs_tot_width * 0.15
+menu_showcase <- (abs_tot_width)/2
+user_story_height <- (abs_tot_width)/2
