cc164060e4d61b1b1c3e69c4cd11b458cc57d2ef
Mohammad Savarmand <msavarmand@protonmail.com>
2023-12-11 09:51:26 -0500

    Comment the height percentage for menu and user stories

--- design/landing-page-dimensions.R
+++ design/landing-page-dimensions.R
@@ -7,8 +7,8 @@ abs_tot_height <- 1400 + (1400*0.2) # 0.05 for header and 0.15 for footer
 # Heights
 header_height <- abs_tot_width * 0.07
 footer_height <- abs_tot_width * 0.13
-menu_showcase <- (abs_tot_width)/2
-user_story_height <- (abs_tot_width)/2
+menu_showcase <- (abs_tot_width)/2 # 0.45
+user_story_height <- (abs_tot_width)/2 # 0.45
 
 heights <- c(header_height, footer_height, menu_showcase, user_story_height)
 sum(heights) == abs_tot_height
