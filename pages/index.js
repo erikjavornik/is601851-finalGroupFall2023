b9a969aec0868f641c26f984f0c108718b696d23
Erik Javornik <ej83@njit.edu>
2023-12-13 10:57:31 -0500

    Removed all refrences to markdown files

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/navbar.js'
-import Date from '../components/date';
-import { getSortedPostsData } from '../lib/menu_posts.js';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -30,20 +20,6 @@ export default function Home({ allPostsData }) {
         <h1>Test</h1>
       </main>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
 
       <footer className={styles.footer}>
         <a
