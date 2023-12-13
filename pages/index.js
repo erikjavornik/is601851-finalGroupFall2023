499823e593ebbb39285b461378c2b86397e1bdd7
Erik Javornik <ej83@njit.edu>
2023-12-13 10:25:55 -0500

    Removed utilStyle refrences

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,13 @@ export default function Home() {
         <h1>Test</h1>
       </main>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section>
+        <h2>Blog</h2>
+        <ul>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+            <li key={id}>
+              <Link href={`/Markdown posts/${id}`}>{title}</Link>
               <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
             </li>
           ))}
         </ul>
