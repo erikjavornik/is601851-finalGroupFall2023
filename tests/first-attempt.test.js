e09662a28fa0738b5a679ce9d85df38d7238f53e
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-16 11:59:45 -0500

    Successfully passed text locator test closes #27

--- tests/first-attempt.test.js
+++ tests/first-attempt.test.js
@@ -1,9 +1,9 @@
 // tests/homepage-content.spec.js
 const { test, expect } = require('@playwright/test');
 
-test('Homepage should have the word "soothing"', async ({ page }) => {
+test('Homepage should display "soothing" from Markdown file', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  // Check if the word "soothing" is present on the page
+  // Check for the presence of the word "soothing" on the page
   await expect(page.locator('text=soothing')).toBeVisible();
 });
