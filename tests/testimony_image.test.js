e1abb50b3c11bd7038c3e7bc2edf6eed498a2e01
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-18 16:44:13 -0500

    pw test 12 check grace pic closes #138

--- tests/testimony_image.test.js
+++ tests/testimony_image.test.js
@@ -11,3 +11,14 @@ test('Check if "Man_Enjoying_Coffee_1.png" is present on the page', async ({ pag
   expect(image).not.toBeNull();
 });
 
+test('Check if "Woman_Enjoying_Coffee_1.png" is present on the page', async ({ page }) => {
+    // Navigate to the page
+    await page.goto('http://localhost:3000/');
+  
+    // Check for an image with the specific src
+    const image = await page.$(`img[src*="Woman_Enjoying_Coffee_1.png"]`);
+  
+    // Assert that the image is present
+    expect(image).not.toBeNull();
+  });
+  
