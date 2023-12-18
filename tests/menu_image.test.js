112decd8285f7a351bdfb99a64b1ae18a9a92646
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-18 16:31:54 -0500

    PW test 11 check chai latte pic closes #133

--- tests/menu_image.test.js
+++ tests/menu_image.test.js
@@ -21,3 +21,14 @@ test('Check if "Creative_Cup.png" is present on the page', async ({ page }) => {
     // Assert that the image is present
     expect(image).not.toBeNull();
   });
+
+  test('Check if "Chai_Latte.png" is present on the page', async ({ page }) => {
+    // Navigate to the page
+    await page.goto('http://localhost:3000/');
+  
+    // Check for an image with the specific src
+    const image = await page.$(`img[src*="Chai_Latte.png"]`);
+  
+    // Assert that the image is present
+    expect(image).not.toBeNull();
+  });
