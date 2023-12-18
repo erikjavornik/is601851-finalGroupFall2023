82f0eb5560248bfa193ba2be0bd366bc0e3e25a7
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-18 16:37:56 -0500

    pw test 12 check for kevin pic closes #136

---
+++ tests/testimony_image.test.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test('Check if "Man_Enjoying_Coffee_1.png" is present on the page', async ({ page }) => {
+  // Navigate to the page
+  await page.goto('http://localhost:3000/');
+
+  // Check for an image with the specific src
+  const image = await page.$(`img[src*="Man_Enjoying_Coffee_1.png"]`);
+
+  // Assert that the image is present
+  expect(image).not.toBeNull();
+});
+
