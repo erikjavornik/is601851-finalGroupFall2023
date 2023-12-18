539344a5ef9167dd9d8cb42f24b9b6bd8eba829d
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-18 17:08:23 -0500

    PW test 17 about pic 3 check closes #147

--- tests/about_picture.test.js
+++ tests/about_picture.test.js
@@ -20,4 +20,15 @@ test('Check if "Creative_1.png" is displayed on the page', async ({ page }) => {
   
     // Assert that the image is present
     expect(image).not.toBeNull();
-  });
+});
+
+  test('Check if "Mountain_1.png" is displayed on the page', async ({ page }) => {
+    // Replace with the URL of the page where the image should be displayed
+    await page.goto('http://localhost:3000/about');
+  
+    // Check for an image with the specific src
+    const image = await page.$(`img[src*="Mountain_1.png"]`);
+  
+    // Assert that the image is present
+    expect(image).not.toBeNull();
+});
