8153ca725348657b448eb5782729d94003b11d91
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-18 13:29:24 -0500

    Create PW test to check for testimoney3 text

--- tests/testimony_text.test.js
+++ tests/testimony_text.test.js
@@ -28,5 +28,22 @@ test('Check for Grace\'s testimonial with quotes on the homepage', async ({ page
   
     // Additionally, check if the text "Kevin" is also present
     await expect(page.locator(`text=${name}`)).toBeVisible({ timeout: 10000 });
-  });
+});
+
+
+
+  test('Check for Jasmine\'s testimonial with quotes on the homepage', async ({ page }) => {
+    // Navigate to the homepage
+    await page.goto('http://localhost:3000');
   
+    // A unique portion of the testimonial text
+    const partialText = "This cafe is my go-to spot for a productive and relaxing coffee break";
+    const name = "Jasmine";
+  
+    // Assert that the partial text is present and visible on the page
+    await expect(page.locator(`text=${partialText}`)).toBeVisible({ timeout: 10000 });
+  
+    // Additionally, check if the text "Kevin" is also present
+    await expect(page.locator(`text=${name}`)).toBeVisible({ timeout: 10000 });
+});
+
