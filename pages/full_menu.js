ceec8a9773f87cd7e11f3ab7dddff8a9fc5a15ff
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-19 12:51:17 -0500

    Create a page for menu closes #165

---
+++ pages/full_menu.js
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/navbar.js'
+// import { getFullMenu } from '../lib/full_menu';
+import GoogleAnalytics from '../components/GoogleAnalyytics.js';
+import CookieBanner from '../components/cookiebanner.js';
