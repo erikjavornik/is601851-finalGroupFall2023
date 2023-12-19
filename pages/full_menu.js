2eda7591f905a97f9cb61d66eba814af982ff4e3
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-19 13:11:53 -0500

    menu page should be created

--- pages/full_menu.js
+++ pages/full_menu.js
@@ -1,6 +1,55 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/navbar.js'
-// import { getFullMenu } from '../lib/full_menu';
-import GoogleAnalytics from '../components/GoogleAnalyytics.js';
-import CookieBanner from '../components/cookiebanner.js';
+import { getFullMenuContent } from '../lib/full_menu_post'; // Updated import
+import GoogleAnalytics from '../components/GoogleAnalytics'; // Corrected component name
+import CookieBanner from '../components/cookiebanner';
+
+export async function getStaticProps() {
+  const menuContent = getFullMenuContent(); // Updated function call
+  
+  return {
+    props: {
+      menuContent,
+    },
+  };
+}
+
+export default function MenuPage({ menuContent }) { 
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>M&E Creative Cup - Menu</title> 
+        <link rel="icon" href="/cafe_logo.jpg" />
+      </Head>
+
+      <GoogleAnalytics GA_MEASUREMENT_ID='G-0F7YYZ8TE4' />
+      <CookieBanner />
+
+      <header className={styles.navbar}>
+        <img src="/cafe_logo.jpg" alt="M&E Creative Cup" className={styles.cafelogo} />
+        <Navbar />
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.gridContainer}>
+          {/* Display the menu content */}
+          <div className={styles.gridItem}>
+            <p>{menuContent.content}</p> {/* Removed image reference */}
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
