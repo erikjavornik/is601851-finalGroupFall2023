9f4d9167967390b298758f5c7491a789b6d382fb
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-19 16:24:50 -0500

    use marked package to format menu page closes #171

--- pages/full_menu.js
+++ pages/full_menu.js
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Navbar from '../components/navbar.js'
+import Navbar from '../components/navbar.js';
 import { getFullMenuContent } from '../lib/full_menu_post'; 
 import GoogleAnalytics from '../components/GoogleAnalytics'; 
 import CookieBanner from '../components/cookiebanner';
+import {marked} from 'marked'; // Use the correct import statement here
 
 export async function getStaticProps() {
   const menuContent = getFullMenuContent(); 
-  
   return {
     props: {
       menuContent,
@@ -16,6 +16,9 @@ export async function getStaticProps() {
 }
 
 export default function MenuPage({ menuContent }) { 
+  const htmlContent = marked(menuContent.content); // Convert Markdown to HTML
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -33,21 +36,15 @@ export default function MenuPage({ menuContent }) {
 
       <main className={styles.main}>
         <div className={styles.gridContainer}>
-          {/* Display the menu content */}
-          <div className={styles.gridItem}>
-            <p>{menuContent.content}</p> 
-          </div>
+          {/* Display the menu content as HTML */}
+          <div className={styles.gridItem} dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank" rel="noopener noreferrer">
+          Powered by <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
     </div>
