f78a6c773bb151ab4fd4fb56b75aae3e10b2bb07
Mohammad Savarmand <80232411+FunkeMonke@users.noreply.github.com>
2023-12-20 15:02:14 -0500

    added mailchimp to about page closes #183

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import Navbar from '../components/navbar.js'
 import { getAboutFiles } from '../lib/about_posts';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 import CookieBanner from '../components/cookiebanner';
+import NewsLetterSignUpForm from '../components/signupform';
 
 export async function getStaticProps() {
     const about = getAboutFiles();
@@ -45,15 +46,8 @@ export default function About({about}) {
             </main>
 
             <footer className={styles.footer}>
-            <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-            </a>
-        </footer>
+        <NewsLetterSignUpForm />
+      </footer>
 
 
         </div>
